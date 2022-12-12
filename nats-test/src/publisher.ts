35e52d23fb47c866fce83cf3c2b0606c897baaa6
Mert Can Benli <mertcanbenli99@outlook.com>
2022-12-12 05:05:29 +0300

    Nats streaming server icin deployment ve servis olusturuldu. Mini bir nats projesi ile event publishlendi ve proje icin diyagramlar eklendi

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,19 @@
+import nats from "node-nats-streaming";
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Connection with NATS is established");
+
+  const data = JSON.stringify({
+    id: "1111",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("event published");
+  });
+});
