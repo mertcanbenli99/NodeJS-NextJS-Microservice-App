5b2a5753e545f222fe44e93984af8a0f9cf495fb
Mert Can Benli <mertcanbenli99@outlook.com>
2022-12-15 10:37:52 +0300

    Listener classları ve event veri tipleri ile yeniden kullanilmaya daha elverisli ve typescirpt ile uyumlu event mesajları eklendi

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Listener } from "./base-listener";
 import { Message } from "node-nats-streaming";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = "payments-service";
 
-  onMessage(data: any, message: Message) {
+  onMessage(data: TicketCreatedEvent["data"], message: Message) {
     console.log("Event data: ", data);
 
     message.ack();
