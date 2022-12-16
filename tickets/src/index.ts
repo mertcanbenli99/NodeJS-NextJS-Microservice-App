ebe1166059eff29e41514af13b9e649f41f1530c
Mert Can Benli <mertcanbenli99@outlook.com>
2022-12-16 07:37:51 +0300

    nats client'ı icin singleton yapısına benzer bir wrapper olusturildi.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.jwt) {
@@ -10,6 +11,18 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect(
+      "ticketing",
+      "randomvaluenow",
+      "http://nats-srv:4222"
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("nats connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongodb");
   } catch (err) {
@@ -18,7 +31,7 @@ const start = async () => {
 };
 
 app.listen(3000, () => {
-  console.log("auth service v4");
+  console.log("tickets service v4");
   console.log("listening on port 3000");
 });
 
