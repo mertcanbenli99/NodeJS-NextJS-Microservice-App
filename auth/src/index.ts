4fa1e4f40e2f59e3cf27699b329d5bbc897e9713
Mert Can Benli <mertcanbenli99@outlook.com>
2022-12-26 22:13:52 +0300

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("starting the server...");
+
   if (!process.env.jwt) {
     throw new Error("Jwt key must be defined");
   }
